bbde1276fa414d67c76f33f7fa2ca3482fdf1829
Vocatann <ivane.lukanov@gmail.com>
2025-07-17 00:55:40 -0400

    feat(ui): outline basic layout

---
+++ src/components/header.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="border-b-4 border-border h-30 flex items-center px-10">
+      <nav>
+        <ul className="flex gap-x-10">
+          <li>
+            <Link href="/" className="text-lg text-accent">Home</Link>
+          </li>
+          <li>
+            <Link href="/about" className="text-lg text-accent">About</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
