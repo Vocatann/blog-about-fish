4425a1f6223765b0451063140d827c8b7b516ef3
Vocatann <ivane.lukanov@gmail.com>
2025-08-21 12:44:55 -0400

    feat(footer): change links colors to primary accent instead of secondary and change disclaimer font size to extra small

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,11 +4,11 @@ export default function Footer() {
   return (
     <footer className="border-t-2 border-border flex flex-col justify-center space-y-5 py-10">
       <ul className="flex space-x-2 justify-center">
-        <li><Link href="/about" className="text-accent-secondary hover:underline">About page</Link></li>
-        <li><a href="https://github.com/Vocatann" target="_blank" className="text-accent-secondary hover:underline">Github</a></li>
-        <li><a href="https://x.com/home" target="_blank" className="text-accent-secondary hover:underline">Twitter</a></li>
+        <li><Link href="/about" className="text-accent hover:underline">About page</Link></li>
+        <li><a href="https://github.com/Vocatann" target="_blank" className="text-accent hover:underline">Github</a></li>
+        <li><a href="https://x.com/home" target="_blank" className="text-accent hover:underline">Twitter</a></li>
       </ul>
-      <span className="text-center">All content on this blog is ai generated</span>
+      <span className="text-center text-xs">All content on this blog is ai generated</span>
     </footer>
   )
 }
