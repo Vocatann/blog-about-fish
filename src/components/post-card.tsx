89371398818c6ac65832ab1180d8bd004de39251
Vocatann <ivane.lukanov@gmail.com>
2025-08-21 12:42:44 -0400

    feat(post-card): make excerpt font small

--- src/components/post-card.tsx
+++ src/components/post-card.tsx
@@ -4,13 +4,13 @@ import strip from 'strip-markdown';
 
 export default async function PostCard({post}: {post : FullBlogPost}) {
   const plainText = await remark().use(strip).process(post.content);
-  const excerpt = plainText.toString().trim().substring(0, 350) + '...';
+  const excerpt = plainText.toString().trim().substring(0, 450) + '...';
 
   return (
     <div className="border-2 border-border rounded-lg p-2 w-[350px]">
       <span className="text-text">{post.metadata.title}</span>
       <hr />
-      <p className="py-2 h-[300px] overflow-hidden whitespace-pre-line">
+      <p className="py-2 h-[300px] overflow-hidden whitespace-pre-line text-sm">
         {excerpt}
       </p>
     </div>
