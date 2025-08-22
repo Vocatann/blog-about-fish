b0c1046514d50a896337efd939c64dd1aaf2bc85
Vocatann <ivane.lukanov@gmail.com>
2025-08-22 16:47:33 -0400

    feat(footer): remove twitter link from footer and change gihub link to blog s repository instead of profile

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,8 +5,7 @@ export default function Footer() {
     <footer className="border-t-2 border-border flex flex-col justify-center space-y-5 py-10">
       <ul className="flex space-x-2 justify-center">
         <li><Link href="/about" className="text-accent hover:underline">About page</Link></li>
-        <li><a href="https://github.com/Vocatann" target="_blank" className="text-accent hover:underline">Github</a></li>
-        <li><a href="https://x.com/home" target="_blank" className="text-accent hover:underline">Twitter</a></li>
+        <li><a href="https://github.com/Vocatann/blog-about-fish" target="_blank" className="text-accent hover:underline">Github</a></li>
       </ul>
       <span className="text-center text-xs">All content on this blog is ai generated</span>
     </footer>
