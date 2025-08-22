d9d6da26290e21e007ee883329977f8cf24609f7
Vocatann <ivane.lukanov@gmail.com>
2025-08-22 09:03:06 -0400

    style: add semicolons

--- src/components/chatbot-window.tsx
+++ src/components/chatbot-window.tsx
@@ -12,7 +12,7 @@ export default function ChatbotWindow() {
   const sendHorizontalSvg = 
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-send-horizontal-icon lucide-send-horizontal">
       <path d="M3.714 3.048a.498.498 0 0 0-.683.627l2.843 7.627a2 2 0 0 1 0 1.396l-2.842 7.627a.498.498 0 0 0 .682.627l18-8.5a.5.5 0 0 0 0-.904z"/><path d="M6 12h16"/>
-    </svg>
+    </svg>;
 
   const botMessageSquareSvg = 
     <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-bot-message-square-icon lucide-bot-message-square">
@@ -27,7 +27,7 @@ export default function ChatbotWindow() {
   const xSvg = 
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-x-icon lucide-x">
       <path d="M18 6 6 18"/><path d="m6 6 12 12"/>
-    </svg>
+    </svg>;
   
   const handleChatOpen = () => setIsChatOpen(prev => !prev);
 
