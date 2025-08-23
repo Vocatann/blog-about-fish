a734388cff94428c2a215ce6ecc203d79c53cba9
Vocatann <ivane.lukanov@gmail.com>
2025-08-23 14:15:50 -0400

    style(svgs): wrap all svgs in parenthesis

--- src/lib/svgs.tsx
+++ src/lib/svgs.tsx
@@ -48,7 +48,7 @@ export const circleLoadingSvg = (
   </svg>
 );
 
-export const threeDotsTypingSvg =
+export const threeDotsTypingSvg = (
   <svg width="40" height="10" viewBox="0 0 60 20" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor">
     <circle cx="10" cy="10" r="5">
       <animate 
@@ -74,25 +74,30 @@ export const threeDotsTypingSvg =
         repeatCount="indefinite" 
         begin="0.4s"/>
     </circle>
-  </svg>;
+  </svg>
+);
 
 
-export const sendHorizontalSvg = 
+export const sendHorizontalSvg = (
   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-send-horizontal-icon lucide-send-horizontal">
     <path d="M3.714 3.048a.498.498 0 0 0-.683.627l2.843 7.627a2 2 0 0 1 0 1.396l-2.842 7.627a.498.498 0 0 0 .682.627l18-8.5a.5.5 0 0 0 0-.904z"/><path d="M6 12h16"/>
-  </svg>;
+  </svg>
+);
 
-export const botMessageSquareSvg = 
+export const botMessageSquareSvg = (
   <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-bot-message-square-icon lucide-bot-message-square">
     <path d="M12 6V2H8"/><path d="m8 18-4 4V8a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2Z"/><path d="M2 12h2"/><path d="M9 11v2"/><path d="M15 11v2"/><path d="M20 12h2"/>
-  </svg>;
+  </svg>
+);
 
-export const squareArrowOutUpLeftSvg = 
+export const squareArrowOutUpLeftSvg = (
   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-square-arrow-out-up-left-icon lucide-square-arrow-out-up-left">
     <path d="M13 3h6a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-6"/><path d="m3 3 9 9"/><path d="M3 9V3h6"/>
-  </svg>;
+  </svg>
+);
 
-export const xSvg = 
+export const xSvg = (
   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-x-icon lucide-x">
     <path d="M18 6 6 18"/><path d="m6 6 12 12"/>
-  </svg>;
+  </svg>
+);
