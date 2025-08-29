e4c4b0417c09d0cd975a9d4c16dca4902ff21c77
Vocatann <ivane.lukanov@gmail.com>
2025-08-29 13:49:07 -0400

    feat(post-card): change background color to card

--- src/components/post-card.tsx
+++ src/components/post-card.tsx
@@ -3,7 +3,7 @@ import { FullBlogPost } from "@/types";
 export default function PostCard({post}: {post : FullBlogPost}) {
 
   return (
-    <div className="border-2 border-border rounded-lg p-2 w-[350px] hover:shadow-xl transition">
+    <div className="bg-card border-2 border-border rounded-lg p-2 w-[350px] hover:shadow-xl transition">
       <span className="text-text">{post.metadata.title}</span>
       <hr />
       <p className="py-2 h-[300px] overflow-hidden whitespace-pre-line text-sm">
